8e9ce9221daf9bf6f008b2a59aeaa11aee582642
Olga Filchukova <makeeva.o.y@gmail.com>
2023-09-08 13:32:57 +0300

    footer created

---
+++ movies-explorer/src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import './Footer.css';
+import '../Link/Link.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__container">
+        <p className="footer__copyright">&copy;2023</p>
+        <nav className="footer__menu">
+          <ul className="footer__links">
+            <li>
+              <a target="_blank" href="https://practicum.yandex.ru" className="footer__link link">Яндекс.Практиткум</a>
+            </li>
+            <li>
+              <a target="_blank" href="https://github.com" className="footer__link link">GitHub</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>)
+}
+
+export default Footer;
