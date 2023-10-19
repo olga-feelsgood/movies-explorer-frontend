9cb0aa3d9235f7094a3d73f6c07aac2ed5895ce1
Olga Filchukova <makeeva.o.y@gmail.com>
2023-10-19 16:36:19 +0300

    bugs after second review fixed

--- movies-explorer/src/components/Profile/Profile.js
+++ movies-explorer/src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Profile.css';
 import '../Section/Section.css';
 import '../Link/Link.css';
@@ -53,13 +54,13 @@ function Profile() {
           <div className='profile__buttons'>
             {isEditMode ?
               <>
-                <span className={!isButtonAble ? 'profile__error profile__error_position_low': 'profile__error profile__error_position_low profile__error_hidden'}>При обновлении профиля произошла ошибка</span>
+                <span className={!isButtonAble ? 'profile__error profile__error_position_low' : 'profile__error profile__error_position_low profile__error_hidden'}>При обновлении профиля произошла ошибка</span>
                 <button className={isButtonAble ? 'profile__save-button link' : 'profile__save-button profile__save-button_disabled link'} type='submit'>Сохранить</button>
               </>
               :
               <>
                 <button className='profile__button profile__button_type_edit link' type='button' onClick={turnOnEditMode}>Редактировать</button>
-                <button className='profile__button profile__button_type_logout link' type='button'>Выйти из аккаунта</button>
+                <Link to='/' className='profile__button profile__button_type_logout link' type='button'>Выйти из аккаунта</Link>
               </>
             }
           </div>
