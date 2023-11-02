3062bd5653da98604402a350bf3ac02ed778434f
Olga Filchukova <makeeva.o.y@gmail.com>
2023-11-02 19:16:59 +0300

    feat saved-movies page

--- movies-explorer/src/components/MoviesCard/Moviescard.js
+++ movies-explorer/src/components/MoviesCard/Moviescard.js
@@ -9,29 +9,35 @@ function MoviesCard(props) {
   const location = useLocation();
 
   function likeOrDislikeMovie() {
-    if(props.isLiked) {
-      props.onDislike(props.savedMovies.find((c)=>c.movieId === props.movie.id))
+    if (props.isLiked) {
+      props.onDislike(props.savedMovies.find((c) => c.movieId === props.movie.id))
     } else {
       props.onLike(props.movie);
     }
   }
 
+function deleteMovie() {
+  props.onDislike(props.movie);
+}
+
   function formatTime(duration) {
     const min = duration % 60;
     const hour = Math.floor(duration / 60);
-    return hour ? `${hour}ч ${min}м`: `${min}м`;
+    return hour ? `${hour}ч ${min}м` : `${min}м`;
   }
+
+  console.log(props.movie.image)
 
   return (
     <li className='movies-card'>
       <a className='movies-card__trailer-link link' target='_blank' rel='noreferrer' href={props.movie.trailerLink}>
-        <img className='movies-card__image' alt='Обложка фильма' src={moviesApi._url + props.movie.image.url} />
+        <img className='movies-card__image' alt='Обложка фильма' src = { location.pathname === '/saved-movies' ? props.movie.image : moviesApi._url + props.movie.image.url } />
       </a>
       <div className='movies-card__box'>
         <div className='movies-card__container'>
           <h2 className='movies-card__title'>{props.movie.nameRU}</h2>
           {location.pathname === '/saved-movies'
-            ? <button type='button' className='movies-card__button movies-card__button_type_delete' />
+            ? <button onClick={deleteMovie} type='button' className='movies-card__button movies-card__button_type_delete' />
             : <button onClick={likeOrDislikeMovie} type='button' className={props.isLiked ? 'movies-card__button movies-card__button_type_save' : 'movies-card__button'} />
           }
         </div>
--- movies-explorer/src/components/SavedMovies/Savedmovies.js
+++ movies-explorer/src/components/SavedMovies/Savedmovies.js
@@ -3,17 +3,61 @@ import SearchForm from '../SearchForm/Searchform.js';
 import MoviesCardList from '../MoviesCardList/Moviescardlist.js';
 import Footer from '../Footer/Footer.js';
 import Header from '../Header/Header.js';
-
+import { filterByTitleMovies, filterShortMovies } from '../../utils/utils';
+import { useState, useEffect } from 'react';
 
 function SavedMovies(props) {
+
+  const [isShort, setIsShort] = useState(false);
+  const [isMoviesNotFound, setIsMoviesNotFound] = useState(false);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [inputQuery, setInputQuery] = useState('');
+
+  function searchAmongSavedMovies(inputQuery) {
+    setInputQuery(inputQuery)
+  }
+
+  function handleShortMovies() {
+    const result = !isShort;
+    setIsShort(result);
+  }
+
+  //Отображать фильмы при изменении чекбокса, поискового запроса
+  useEffect(() => {
+    const filteredSavedMovies = filterByTitleMovies(props.savedMovies, inputQuery)
+    setFilteredMovies(isShort ? filterShortMovies(filteredSavedMovies) : filteredSavedMovies);
+  }, [props.savedMovies, isShort, inputQuery])
+
+  //Менять переменную состояния, если не найдены фильмы
+  useEffect(() => {
+    if (isShort) {
+      const filteredSavedMovies = filterByTitleMovies(props.savedMovies, inputQuery);
+      const shortFilteredSavedMovies = filterShortMovies(filteredSavedMovies);
+      setIsMoviesNotFound(shortFilteredSavedMovies.length === 0 && inputQuery !== '');
+    } else {
+      const filteredSavedMovies = filterByTitleMovies(props.savedMovies, inputQuery);
+      setIsMoviesNotFound(filteredSavedMovies.length === 0 && inputQuery !== '');
+    }
+  }, [props.savedMovies, isShort, inputQuery])
+
   return (
     <>
       <div className='wrapper'>
         <Header
           isLoggedIn={props.isLoggedIn} />
         <main className='saved-movies'>
-          <SearchForm />
-          <MoviesCardList />
+          <SearchForm
+            onSearch={searchAmongSavedMovies}
+            isShort={isShort}
+            handleIsShort={handleShortMovies}
+          />
+          <MoviesCardList
+            movies={filteredMovies}
+            isMoviesNotFound={isMoviesNotFound}
+            onLike={props.onLike}
+            savedMovies={props.savedMovies}
+            onDislike={props.onDislike}
+          />
         </main>
       </div>
       <Footer />
