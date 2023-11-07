6577b9de1bbc4ec7124bac29469ca03deef27177
Olga Filchukova <makeeva.o.y@gmail.com>
2023-11-07 17:30:14 +0300

    fix protected route navigation route

--- movies-explorer/src/utils/ProtectedRoute.js
+++ movies-explorer/src/utils/ProtectedRoute.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ element: Component, ...props }) {
   return (
-  props.isLoggedIn ? <Component {...props} /> : <Navigate to="/signin" replace/>
+  props.isLoggedIn ? <Component {...props} /> : <Navigate to="/" replace/>
   )
 }
 
